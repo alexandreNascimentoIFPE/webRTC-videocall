5a1718b371c3107f55a279f6e82a39bc6d254a2d
alexandreNascimentoIFPE <alexandre.nascimentoti@gmail.com>
2020-05-11 17:48:53 -0300

    oi

--- server.js
+++ server.js
@@ -1,31 +1,52 @@
-const express = require('express');
+require('dotenv').config();
+const express = require("express");
+const http = require("http");
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
 
-// port setup and listening 
-http.listen(PORT, () => {
-    console.log("listening on port " + PORT)
+const users = {};
+
+const socketToRoom = {};
+
+io.on('connection', socket => {
+    socket.on("join room", roomID => {
+        if (users[roomID]) {
+            const length = users[roomID].length;
+            if (length === 4) {
+                socket.emit("room full");
+                return;
+            }
+            users[roomID].push(socket.id);
+        } else {
+            users[roomID] = [socket.id];
+        }
+        socketToRoom[socket.id] = roomID;
+        const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
+
+        socket.emit("all users", usersInThisRoom);
+    });
+
+    socket.on("sending signal", payload => {
+        io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
+    });
+
+    socket.on("returning signal", payload => {
+        io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
+    });
+
+    socket.on('disconnect', () => {
+        const roomID = socketToRoom[socket.id];
+        let room = users[roomID];
+        if (room) {
+            room = room.filter(id => id !== socket.id);
+            users[roomID] = room;
+        }
+    });
+
 });
 
-// serving our index.html 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/index.html")
-})
-// allows files in this folder to be served all the time
-app.use(express.static('public'));
+server.listen(process.env.PORT || 8000, () => console.log('server is running on port 8000'));
 
-// init socket 
-io.on('connection', function (socket) {
-    console.log("client is connected " + socket.id)
-    // receive the event, then send data to clients
-    socket.on('userMessage', (data) => {
-        io.sockets.emit("userMessage", data)
-    })
-    // receive the typing event, send out to clients
-    socket.on('userTyping', (data) => {
-        socket.broadcast.emit('userTyping', data)
 
-    });
-})
